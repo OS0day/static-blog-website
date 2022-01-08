ba573960249349f392cd411e91e4f54c9494012c
OS0day <47381660+OS0day@users.noreply.github.com>
2022-01-08 10:28:46 +0000

    Deploying to gh-pages from  @ b5888c04022d72ae38678f489c5f6d15779afeb8 🚀

--- .docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require("G:\\IT成长知识库\\AA_大学期间接手的项目\\Gitee_demo仓库集合\\KnowledgeAndBlog\\static-blog-website\\node_modules\\infima\\dist\\css\\default\\default.css"),
-  require("G:\\IT成长知识库\\AA_大学期间接手的项目\\Gitee_demo仓库集合\\KnowledgeAndBlog\\static-blog-website\\node_modules\\@docusaurus\\theme-classic\\lib\\prism-include-languages"),
-  require("G:\\IT成长知识库\\AA_大学期间接手的项目\\Gitee_demo仓库集合\\KnowledgeAndBlog\\static-blog-website\\node_modules\\@docusaurus\\theme-classic\\lib\\admonitions.css"),
-  require("G:\\IT成长知识库\\AA_大学期间接手的项目\\Gitee_demo仓库集合\\KnowledgeAndBlog\\static-blog-website\\src\\css\\custom.css"),
-];
